64e5969d87515af7769909de9bddc0e7fa6c87c4
Gorgai <gorgaiswag@gmail.com>
2024-07-28 17:20:02 +0200

    Fungerande hittils
    Hyfsat fungerande minesweeper

---
+++ wwwroot/localStorage.js
@@ -0,0 +1,17 @@
+﻿export function setItem(key, value) {
+    localStorage.setItem(key, value);
+}
+
+export function getItem(key) {
+    return localStorage.getItem(key);
+}
+
+export function preventDefaultContextMenu(element) {
+    if (element instanceof HTMLElement) {
+        element.addEventListener("contextmenu", function (e) {
+            e.preventDefault();
+        });
+    } else {
+        console.error("Invalid element passed to preventDefaultContextMenu");
+    }
+}
