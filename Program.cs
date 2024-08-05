0eeabbcb1a779c8c4bf30eac7174d16438acf917
Gorgai <gorgaiswag@gmail.com>
2024-08-05 08:19:37 +0200

    Jobbar på v1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using genegames;
+using genegames.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.JSInterop;
@@ -7,6 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped<GameStateService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
