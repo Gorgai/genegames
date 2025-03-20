d420ce89625a5060f697cb6b93948abc96848410
Gorgai <gorgaiswag@gmail.com>
2025-03-20 19:23:11 +0100

    asd

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using genegames;
+using genegames.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<GameStateService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
